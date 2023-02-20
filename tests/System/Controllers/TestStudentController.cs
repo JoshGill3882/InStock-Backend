71afdbbd83bc24572d9d9186879187a975a86773
c2035950 <miaham@cardiff.ac.uk>
2023-02-20 13:45:35 +0000

    Install xUnit, Moq and FluentAssertions for unit testing. Add a simple unit test for StudentController

---
+++ tests/System/Controllers/TestStudentController.cs
@@ -0,0 +1,25 @@
+﻿using FluentAssertions;
+using instock_server_application.Controllers;
+using instock_server_application.Service;
+using instock_server_application.tests.MockData;
+using Moq;
+using Xunit;
+
+namespace instock_server_application.tests.System.Controllers; 
+
+public class TestStudentController {
+
+    [Fact]
+    public void GetAllStudents_ReturnsListOfStudents() {
+        // Arrange
+        var studentService = new Mock<IStudentService>();
+        studentService.Setup(_ => _.GetAllStudents()).Returns(MockStudents.MockStudentData());
+        var studentController = new StudentController();
+        
+        // Act
+        var result = studentController.GetAllStudents();
+        
+        // Assert
+        result.Should().BeEquivalentTo(MockStudents.MockStudentData());
+    }
+}
