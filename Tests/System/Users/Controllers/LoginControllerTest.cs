f8f94ac560574e019d6ee4681bd3b304616d30e4
c2035950 <miaham@cardiff.ac.uk>
2023-02-22 22:55:25 +0000

    Add unit test for login controller

---
+++ Tests/System/Users/Controllers/LoginControllerTest.cs
@@ -0,0 +1,33 @@
+﻿using FluentAssertions;
+using instock_server_application.Users.Controllers;
+using instock_server_application.Users.Services.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+using Xunit;
+using static instock_server_application.tests.System.Users.MockData.UserMock;
+
+namespace instock_server_application.tests.System.Users.Controllers; 
+
+public class LoginControllerTest {
+
+    [Fact]
+    public void Test_Login_ReturnsJwtToken() {
+        // Arrange
+        var token = "jwt-t0k3n";
+        var email = "johnbarnes@gmail.com";
+        var mockService = new Mock<ILoginService>();
+        mockService.Setup(service => service.CreateToken(email)).Returns(token);
+        mockService.Setup(_ => _.FindUserByEmail(email)).Returns(Task.FromResult(SingleUser()));
+        var controller = new LoginController(mockService.Object);
+
+        // Act
+        var result = controller.Login(email, "Test123");
+        
+        // Assert
+        Assert.IsAssignableFrom<Task<IActionResult>>(result);
+        
+        var okObjectResult = result.Result as OkObjectResult;
+        okObjectResult.StatusCode.Should().Be(200);
+        okObjectResult.Value.Should().Be(token);
+    }
+}
---
+++ Tests/System/Users/MockData/UserMock.cs
@@ -0,0 +1,17 @@
+﻿using instock_server_application.Users.Models;
+
+namespace instock_server_application.tests.System.Users.MockData; 
+
+public static class UserMock {
+    public static User SingleUser() {
+        return new User(
+            "johnbarnes@gmail.com", 
+            "Active", 
+            1676904523, 
+            "John", 
+            "Barnes", 
+            "Test123", 
+            "StandardUser"
+        );
+    }
+}
