438a24a941486eada62d149294627c0af941f419
c2035950 <miaham@cardiff.ac.uk>
2023-02-23 15:14:50 +0000

    Add unit test for testing incorrect details

--- Tests/Users/Controllers/LoginControllerTest.cs
+++ Tests/Users/Controllers/LoginControllerTest.cs
@@ -37,15 +37,48 @@ public class LoginControllerTest {
     }
     
     [Fact]
-    public void Test_Login_ReturnsNotFound() {
+    public void Test_Login_ReturnsNotFoundIfEmailIncorrect() {
         // Arrange
+        const string token = "jwt-t0k3n";
+        const string email = "johnbarnes@gmail.com";
+        const string plainTextPassword = "Test123";
+        
         var mockLoginService = new Mock<ILoginService>();
         var mockPasswordService = new Mock<IPasswordService>();
+        mockLoginService.Setup(service => service.CreateToken(email)).Returns(token);
+        mockLoginService.Setup(service => service.FindUserByEmail(email)).Returns(Task.FromResult(SingleUser()));
+        mockPasswordService.Setup(service => service.Verify(plainTextPassword, SingleUser().Password)).Returns(true);
         
         var controller = new LoginController(mockLoginService.Object, mockPasswordService.Object);
 
         // Act
-        var result = controller.Login("","Test123");
+        var result = controller.Login("test@test.com", plainTextPassword);
+        
+        // Assert
+        Assert.IsAssignableFrom<Task<IActionResult>>(result);
+        
+        var notFoundObjectResult = result.Result as NotFoundObjectResult;
+        notFoundObjectResult.StatusCode.Should().Be(404);
+        notFoundObjectResult.Value.Should().Be("Invalid Credentials");
+    }
+    
+    [Fact]
+    public void Test_Login_ReturnsNotFoundIfPasswordIncorrect() {
+        // Arrange
+        const string token = "jwt-t0k3n";
+        const string email = "johnbarnes@gmail.com";
+        const string plainTextPassword = "Test123";
+        
+        var mockLoginService = new Mock<ILoginService>();
+        var mockPasswordService = new Mock<IPasswordService>();
+        mockLoginService.Setup(service => service.CreateToken(email)).Returns(token);
+        mockLoginService.Setup(service => service.FindUserByEmail(email)).Returns(Task.FromResult(SingleUser()));
+        mockPasswordService.Setup(service => service.Verify(plainTextPassword, SingleUser().Password)).Returns(true);
+        
+        var controller = new LoginController(mockLoginService.Object, mockPasswordService.Object);
+
+        // Act
+        var result = controller.Login(email, "incorrectPassword");
         
         // Assert
         Assert.IsAssignableFrom<Task<IActionResult>>(result);
