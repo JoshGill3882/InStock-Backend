b5eee919e6e1860b4cdccf9617cb38c9dcd92173
Samuel Toland <TolandSO@Cardiff.ac.uk>
2023-02-27 02:09:42 +0000

    Model for business

---
+++ Businesses/Models/BusinessModel.cs
@@ -0,0 +1,25 @@
+﻿using Amazon.DynamoDBv2.DataModel;
+
+namespace instock_server_application.Businesses.Models; 
+
+[DynamoDBTable("Businesses")]
+public class BusinessModel {
+    [DynamoDBHashKey] 
+    public int BusinessId { get; set; }
+
+    [DynamoDBProperty("Name")]
+    public string BusinessName { get; set; }
+    
+    [DynamoDBProperty("Owner")] 
+    public string OwnerId { get; set; }
+    
+    [DynamoDBProperty("Members")]
+    public List<string> MembersIdList { get; set; }
+    
+    public BusinessModel(int id, string name, string owner, List<string> membersList) {
+        BusinessId = id;
+        BusinessName = name;
+        OwnerId = owner;
+        MembersIdList = membersList;
+    }
+}
