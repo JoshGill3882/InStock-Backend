c604c3136711a60b9593da742734e769f567f02b
c2040622 <tolandso@cardiff.ac.uk>
2023-02-27 18:46:34 +0000

    UserDTO for passing user details

---
+++ Businesses/Dtos/UserDTO.cs
@@ -0,0 +1,11 @@
+﻿namespace instock_server_application.Businesses.Dtos; 
+
+public class UserDTO {
+    private string UserId { get; set; } 
+    private string UserBusinessId { get; set; }
+    
+    public UserDTO(string userId, string userBusinessId) {
+        UserId = userId;
+        UserBusinessId = userBusinessId;
+    }
+}
