d5eb36a23a05c550684259f02a05bc4a754f82c2
Samuel Toland <TolandSO@Cardiff.ac.uk>
2023-02-27 23:27:53 +0000

    Added validation to Create Business in Business Service

--- Businesses/Services/BusinessService.cs
+++ Businesses/Services/BusinessService.cs
@@ -1,12 +1,36 @@
 ﻿using instock_server_application.Businesses.Dtos;
 using instock_server_application.Businesses.Models;
+using instock_server_application.Businesses.Repositories;
+using instock_server_application.Businesses.Repositories.Interfaces;
 
 namespace instock_server_application.Businesses.Services; 
 
 public class BusinessService : IBusinessService {
+    private IBusinessRepository _businessRepository;
 
-    public bool CreateBusiness(UserDTO userDto, CreateBusinessDto newBusiness) {
+    public BusinessService(IBusinessRepository businessRepository) {
+        _businessRepository = businessRepository;
+    }
 
+    public bool CreateBusiness(UserDto userDto, CreateBusinessDto newBusiness) {
+        
+        // Check if the user Id is valid
+        if (string.IsNullOrEmpty(userDto.UserId)) {
+            return false; // Invalid UserId
+        }
+        
+        // Check if the user has already got a business
+        if (!string.IsNullOrEmpty(userDto.UserBusinessId)) {
+            return false; // Already has a business
+        }
+        
+        // Check if the business is within the character limit of 20 (same as Etsy & Shopify)
+        if (userDto.UserBusinessId.Length > 20) {
+            return false; // Business Name too long
+        }
+        
+        
+            
         return true;
     }
     
