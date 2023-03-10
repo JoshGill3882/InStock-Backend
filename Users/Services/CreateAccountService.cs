c066d733d682e02dd30d9a897c9dda42e9752fe7
@c2038058 <gillj8@cardiff.ac.uk>
2023-03-10 12:08:45 +0000

    Extra bug with email fixed where wasn't stored as lowercase and broke some things

--- Users/Services/CreateAccountService.cs
+++ Users/Services/CreateAccountService.cs
@@ -36,7 +36,7 @@ public class CreateAccountService : ICreateAccountService {
 
         UserDto newUser = new UserDto(
             _utilService.GenerateUUID(),
-            newAccountDto.Email,
+            newAccountDto.Email.ToLower(),
             "Active",
             DateTimeOffset.UtcNow.ToUnixTimeSeconds(),
             newAccountDto.FirstName,
