6105edeb7fe6d513e3d3862882d506868292e1e5
Samuel Toland <TolandSO@Cardiff.ac.uk>
2023-03-20 14:24:03 +0000

    Adds converter for StockUpdateObject, to convert it into DynamoDB supported type

--- Businesses/Models/ItemStockUpdateModel.cs
+++ Businesses/Models/ItemStockUpdateModel.cs
@@ -1,6 +1,9 @@
 ﻿using Amazon.DynamoDBv2.DataModel;
+using Amazon.DynamoDBv2.DocumentModel;
 using Amazon.DynamoDBv2.Model;
 using instock_server_application.Businesses.Dtos;
+using Newtonsoft.Json;
+using JsonSerializer = System.Text.Json.JsonSerializer;
 
 namespace instock_server_application.Businesses.Models; 
 
@@ -14,7 +17,7 @@ public class ItemStockUpdateModel {
     [DynamoDBProperty("BusinessId")]
     public string BusinessId { get; set; }
 
-    [DynamoDBProperty("StockUpdates")]
+    [DynamoDBProperty(typeof(StockUpdateObjectConverter))]
     public List<StockUpdateObject> StockUpdates { get; set; }
 
     
@@ -32,10 +35,8 @@ public class ItemStockUpdateModel {
     }
     
     public class StockUpdateObject {
-        [DynamoDBProperty("StockAmountChanged")]
         public int AmountChanged { get; }
         
-        [DynamoDBProperty("ReasonForStockChange")]
         public string ReasonForChange { get; }
 
         public StockUpdateObject() {
@@ -44,6 +45,35 @@ public class ItemStockUpdateModel {
         public StockUpdateObject(int amountChanged, string reasonForChange) {
             AmountChanged = amountChanged;
             ReasonForChange = reasonForChange;
+        }
+    }
+    public class StockUpdateObjectConverter : IPropertyConverter
+    {
+        public DynamoDBEntry ToEntry(object value)
+        {
+            List<StockUpdateObject> stockUpdate = value as List<StockUpdateObject> ?? throw new ArgumentOutOfRangeException();
+
+            string jsonData = JsonSerializer.Serialize(stockUpdate);
+
+            DynamoDBEntry entry = new Primitive
+            {
+                Value = jsonData
+            };
+            return entry;
+        }
+
+        public object FromEntry(DynamoDBEntry entry)
+        {
+            Primitive? primitive = entry as Primitive;
+            
+            if (primitive == null || !(primitive.Value is String) || string.IsNullOrEmpty((string)primitive.Value))
+                throw new ArgumentOutOfRangeException();
+
+            string jsonData = (string) primitive.Value;
+            
+            List<StockUpdateObject> complexData = JsonSerializer.Deserialize<List<StockUpdateObject>>(jsonData) ?? throw new ArgumentOutOfRangeException();
+            
+            return complexData;
         }
     }
 }
