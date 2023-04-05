8331739f2899981609cf7d2e93904f3b34d3a1a9
C2061455 <okeefesj@cardiff.ac.uk>
2023-04-05 14:53:02 +0100

    Add test for incorrect business id

--- Tests/Businesses/Controllers/StatisticsControllerTest.cs
+++ Tests/Businesses/Controllers/StatisticsControllerTest.cs
@@ -46,4 +46,34 @@ public class StatisticsControllerTest
         okResult.StatusCode.Should().Be(200);
         okResult.Value.Should().Be(expected);
     }
+    
+    [Fact]
+    public void Test_GetItem_NoAccessForIncorrectBusinessId() {
+        // Arrange
+        String incorrectBusinessId = "test123";
+        const string userId = "UID123";
+
+        AllStatsDto expected = null;
+        var mockUser = new ClaimsPrincipal(
+            new ClaimsIdentity(
+                new List<Claim>() {
+                    new Claim("Id", userId),
+                    new Claim("BusinessId", incorrectBusinessId)
+                }, "mockUserAuth"));
+        var mockStatisticsService = new Mock<IStatisticsService>();
+        mockStatisticsService.Setup(service => service.GetStats(It.IsAny<UserDto>(), incorrectBusinessId)).Returns(Task.FromResult(expected)!);
+        
+        var controller = new StatisticsController(mockStatisticsService.Object);
+        controller.ControllerContext = new ControllerContext() {
+            HttpContext = new DefaultHttpContext() { User = mockUser }
+        };
+        
+        // Act
+        var result = controller.GetStatistics(incorrectBusinessId);
+        
+        // Assert
+        Assert.IsAssignableFrom<Task<IActionResult>>(result);
+        var notFoundResult = result.Result as UnauthorizedResult;
+        notFoundResult.StatusCode.Should().Be(401);
+    }
 }
