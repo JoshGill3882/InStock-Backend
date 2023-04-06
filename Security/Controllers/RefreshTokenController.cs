b201dcd19062680c0ce9a44875049572a55aed8e
@c2038058 <gillj8@cardiff.ac.uk>
2023-04-06 19:58:27 +0100

    Method created for getting an Access Token when a Refresh Token is passed in

---
+++ Security/Controllers/RefreshTokenController.cs
@@ -0,0 +1,22 @@
+﻿using instock_server_application.Security.Services.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace instock_server_application.Security.Controllers; 
+
+[ApiController]
+[Route("/refresh")]
+public class RefreshTokenController : ControllerBase {
+    private readonly IAccessTokenService _accessTokenService;
+
+    public RefreshTokenController(IAccessTokenService accessTokenService) {
+        _accessTokenService = accessTokenService;
+    }
+
+    [HttpGet]
+    [Route("{refreshToken}")]
+    public async Task<IActionResult> CreateAccessFromRefresh([FromRoute] string refreshToken) {
+        var result = _accessTokenService.RefreshToAccess(refreshToken);
+
+        return result.Equals("Refresh Token Invalid") ? Unauthorized() : Ok(result);
+    }
+}
