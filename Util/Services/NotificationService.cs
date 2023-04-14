d7f9fd5e01540960c4db84c62684e9da1d4c6c9b
@c2038058 <gillj8@cardiff.ac.uk>
2023-04-14 18:08:31 +0100

    Notification Service updated to pass in ImageURL with Notification Body

--- Util/Services/NotificationService.cs
+++ Util/Services/NotificationService.cs
@@ -1,4 +1,5 @@
 ﻿using FirebaseAdmin.Messaging;
+using instock_server_application.AwsS3.Services.Interfaces;
 using instock_server_application.Businesses.Dtos;
 using instock_server_application.Businesses.Repositories.Interfaces;
 using instock_server_application.Util.Dto;
@@ -8,9 +9,11 @@ namespace instock_server_application.Util.Services;
 
 public class NotificationService : INotificationService {
     private readonly IBusinessRepository _businessRepository;
+    private readonly IStorageService _storageService;
 
-    public NotificationService(IBusinessRepository businessRepository) {
+    public NotificationService(IBusinessRepository businessRepository, IStorageService storageService) {
         _businessRepository = businessRepository;
+        _storageService = storageService;
     }
 
     public NotificationService() {
@@ -40,14 +43,15 @@ public class NotificationService : INotificationService {
         return new NotificationPatternDto(
             new Notification {
                 Title = title,
-                Body = $"You are {title.ToLower()} on: {itemDto.Name}"
+                Body = $"You are {title.ToLower()} on {itemDto.Name}"
             },
             new Dictionary<string, string> {
                 { "SKU", itemDto.SKU },
                 { "BusinessId", itemDto.BusinessId },
                 { "Category", itemDto.Category },
                 { "Name", itemDto.Name },
-                { "Stock", itemDto.TotalStock.ToString() }
+                { "Stock", itemDto.TotalStock.ToString() },
+                { "ImageUrl", itemDto.ImageFilename != null ? _storageService.GetFilePresignedUrl("instock-item-images", itemDto.ImageFilename ?? "").Message : "" }
             }
         );
     }
