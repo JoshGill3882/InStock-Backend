d8eb2bd9c40b7889840b46a4756b4952118a468c
Samuel Toland <TolandSO@Cardiff.ac.uk>
2023-04-14 16:32:08 +0100

    Removes unneeded setter on form

--- Businesses/Controllers/Forms/CreateItemOrderForm.cs
+++ Businesses/Controllers/Forms/CreateItemOrderForm.cs
@@ -1,7 +1,7 @@
 namespace instock_server_application.Businesses.Controllers.forms;
 
 public class CreateItemOrderForm {
-    public string AmountOrdered { get; set; }
+    public string AmountOrdered { get; }
 
     public CreateItemOrderForm(string amountOrdered) {
         AmountOrdered = amountOrdered;
