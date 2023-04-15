1fe1e428612cf470afe7b12b0930c3713c820bb6
Samuel Toland <TolandSO@Cardiff.ac.uk>
2023-04-15 03:18:36 +0100

    Test Controller for item connections

---
+++ Businesses/Services/Interfaces/IItemConnectionService.cs
@@ -0,0 +1,12 @@
+﻿using instock_server_application.Businesses.Dtos;
+using instock_server_application.Security.Services;
+using instock_server_application.Shared.Dto;
+
+namespace instock_server_application.Businesses.Services; 
+
+public interface IItemConnectionService {
+
+    public Task ConnectItem(UserAuthorisationDto userAuthorisationDto,
+        ItemConnectionRequestDto itemConnectionRequestDto);
+
+}
