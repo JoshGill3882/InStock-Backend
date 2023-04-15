7f942ec9dd744428fe0564f11765e64436676ebb
c2035950 <miaham@cardiff.ac.uk>
2023-04-15 01:45:33 +0100

    Create Milestone checker service method in notifications to trigger notifs when total sale of item reaches specific milestones

--- Util/Services/NotificationService.cs
+++ Util/Services/NotificationService.cs
@@ -10,10 +10,12 @@ namespace instock_server_application.Util.Services;
 public class NotificationService : INotificationService {
     private readonly IBusinessRepository _businessRepository;
     private readonly IStorageService _storageService;
+    private readonly IItemRepo _itemRepo;
 
-    public NotificationService(IBusinessRepository businessRepository, IStorageService storageService) {
+    public NotificationService(IBusinessRepository businessRepository, IStorageService storageService, IItemRepo itemRepo) {
         _businessRepository = businessRepository;
         _storageService = storageService;
+        _itemRepo = itemRepo;
     }
 
     public NotificationService() {
@@ -24,6 +26,34 @@ public class NotificationService : INotificationService {
         if (availableStock <= 5 && availableStock > 0)
             SendNotification(CreateNotification("Low on Stock", itemDto), itemDto.BusinessId);
         else if (availableStock == 0) SendNotification(CreateNotification("Out of Stock", itemDto), itemDto.BusinessId);
+    }
+
+    public async void MilestoneNotificationChecker(StoreItemDto itemDto) {
+        List<StatItemDto> statItemDtos = await _itemRepo.GetAllItemsStatsDetails(itemDto.BusinessId);
+        int totalSales = itemDto.TotalOrders;
+        foreach (var statItemDto in statItemDtos) {
+            foreach (var statStockDto in statItemDto.StockUpdates ?? Enumerable.Empty<StatStockDto>()) {
+                if (statStockDto.ReasonForChange == "Sale") {
+                    totalSales += -statStockDto.AmountChanged;
+                }
+            }
+        }
+        Console.Write($"{itemDto.Name} has {totalSales} sales\n");
+
+        switch (totalSales) {
+            case 10:
+                SendNotification(CreateMilestoneNotification(10, itemDto), itemDto.BusinessId);
+                break;
+            case 50:
+                SendNotification(CreateMilestoneNotification(50, itemDto), itemDto.BusinessId);
+                break;
+            case 100:
+                SendNotification(CreateMilestoneNotification(100, itemDto), itemDto.BusinessId);
+                break;
+            case 200:
+                SendNotification(CreateMilestoneNotification(200, itemDto), itemDto.BusinessId);
+                break;
+        }
     }
 
     private void SendNotification(NotificationPatternDto notification, string businessId) {
@@ -45,6 +75,23 @@ public class NotificationService : INotificationService {
             new Notification {
                 Title = title,
                 Body = $"You are {title.ToLower()} on {itemDto.Name}"
+            },
+            new Dictionary<string, string> {
+                { "SKU", itemDto.SKU },
+                { "BusinessId", itemDto.BusinessId },
+                { "Category", itemDto.Category },
+                { "Name", itemDto.Name },
+                { "Stock", itemDto.TotalStock.ToString() },
+                { "ImageUrl", itemDto.ImageFilename != null ? _storageService.GetFilePresignedUrl("instock-item-images", itemDto.ImageFilename ?? "").Message : "" }
+            }
+        );
+    }
+    
+    private NotificationPatternDto CreateMilestoneNotification(int totalSales, StoreItemDto itemDto) {
+        return new NotificationPatternDto(
+            new Notification {
+                Title = "Congrats! You achieved a milestone 🎉",
+                Body = $"You have hit {totalSales} sales on {itemDto.Name} 🥳"
             },
             new Dictionary<string, string> {
                 { "SKU", itemDto.SKU },
